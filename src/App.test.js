5df4c5339149997e6b17b2ee8c5523fd8ce57412
Cédrick Guillet <cguillet@diffusion.cc>
2021-02-26 21:46:30 -0500

    oups

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/I was changed on the feature branch!/i);
